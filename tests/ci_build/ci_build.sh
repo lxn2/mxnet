dcc2220b4f3c406c18e84c006f6c83fad295f98c
Nguyen <lynguyen@amazon.com>
2017-05-30 17:30:31 -0700

    Debug

--- tests/ci_build/ci_build.sh
+++ tests/ci_build/ci_build.sh
@@ -131,6 +131,6 @@ ${DOCKER_BINARY} run --rm --pid=host \
     -e "CI_BUILD_GID=$(id -g)" \
     ${CI_DOCKER_EXTRA_PARAMS[@]} \
     ${DOCKER_IMG_NAME} \
-    "bash -c 'ls *'"
+    bash -c "ls *"
     #${PRE_COMMAND} \
     #${COMMAND[@]}
